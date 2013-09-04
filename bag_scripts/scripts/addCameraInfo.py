9be9a83c852b251888ff554b7fb5a0021c829902
Ross Kidson <ross.kidson@gmail.com>
2013-09-04 16:17:57 -0700

    added input output filenames as params

--- bag_scripts/scripts/addCameraInfo.py
+++ bag_scripts/scripts/addCameraInfo.py
@@ -1,12 +1,21 @@
 #!/usr/bin/env python
 
+
+import sys
 import roslib; roslib.load_manifest('bag_scripts')
 import rospy
 import rosbag
 
+if len(sys.argv) < 2 :
+  print "please provide 2 arguments: addCameraInfo <input.bag> <output.bag>"
+  sys.exit()
+
+input_file = str(sys.argv[1])
+output_file = str(sys.argv[2])
+
 i = 0
-with rosbag.Bag('output.bag', 'w') as output:
-  for topic, msg, t in rosbag.Bag('input.bag').read_messages():
+with rosbag.Bag(output_file, 'w') as output:
+  for topic, msg, t in rosbag.Bag(input_file).read_messages():
     i += 1
     if not i % 100:
       rospy.loginfo(i)
